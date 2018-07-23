823777410b42a088fd0c126f8e65f42b7a14f234
Josh Curry <joshuacurry2615@gmail.com>
2018-07-23 14:58:37 -0400

    Prints the translation of each of the labels

--- Practice.py
+++ Practice.py
@@ -1,47 +1,46 @@
+
 # Imports the Google Cloud client library
 from google.cloud import translate
+
 #Imports the bas64 library to encode images
 import base64
 
+#Imports necessary libraries for google vision API
+import io
+import os
+from google.cloud import vision
+from google.cloud.vision import types
+
 # Instantiates a client
-client = translate.Client()
+translate_client = translate.Client()
 
-print(client.translate('koszula'))
+# Instantiates a client
+vision_client = vision.ImageAnnotatorClient()
+
+# The name of the image file to annotate
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'Earth.jpg')
+
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+
+image = types.Image(content=content)
+
+# Performs label detection on the image file
+response = vision_client.label_detection(image=image)
+labels = response.label_annotations
+
+print('Labels:')
+for label in labels:
+    translation = translate_client.translate(label.description, target_language = 'es')
+    translatedText = translation["translatedText"]
+    print("\n" + translation["input"])
+    print("Translation: " + translatedText)
 
 
 
-
-
-
-
-# import io
-# import os
-
-# # Imports the Google Cloud client library
-# from google.cloud import vision
-# from google.cloud.vision import types
-
-# # Instantiates a client
-# client = vision.ImageAnnotatorClient()
-
-# # The name of the image file to annotate
-# file_name = os.path.join(
-#     os.path.dirname(__file__),
-#     'Earth.jpg')
-
-# # Loads the image into memory
-# with io.open(file_name, 'rb') as image_file:
-#     content = image_file.read()
-
-# image = types.Image(content=content)
-
-# # Performs label detection on the image file
-# response = client.label_detection(image=image)
-# labels = response.label_annotations
-
-# print('Labels:')
-# for label in labels:
-#     print(label.description)
 
 
 
@@ -59,3 +58,4 @@ print(client.translate('koszula'))
 # image_64_decode = base64.decodestring(image_64_encode)
 # image_result = open('Earth.jpg', 'wb')
 # image_result.write(image_64_decode)
+
