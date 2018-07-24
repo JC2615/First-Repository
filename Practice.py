027b5b31dadbae9c2c9582c62394f3eb87f39b86
Josh Curry <joshuacurry2615@gmail.com>
2018-07-24 11:39:50 -0400

    Made a simple user input prompt. Encapsulated most things into functions.

--- Practice.py
+++ Practice.py
@@ -1,9 +1,10 @@
+global num, transNum, userImage, userTranslateLanguage, transLang
+num = 0
+transNum = 0
+transLang = ""
 
 # Imports the Google Cloud client library
 from google.cloud import translate
-
-#Imports the bas64 library to encode images
-import base64
 
 #Imports necessary libraries for google vision API
 import io
@@ -11,39 +12,81 @@ import os
 from google.cloud import vision
 from google.cloud.vision import types
 
-from google.cloud.vision_v1 import ImageAnnotatorClient
+userImage = input("Enter and image url: ")
+userTranslateLanguage = input("Enter a number that corresponds to the language you'd like the output to be translated to - [1] Spanish, [2] Japanese, [3] French, [4] Italian, [5] German: ")
 
-# Instantiates a client
-translate_client = translate.Client()
+if userTranslateLanguage == "1":
+    userTranslateLanguage = "es-US"
+elif userTranslateLanguage == "2":
+    userTranslateLanguage = "ja-JP"
+elif userTranslateLanguage == "3":
+    userTranslateLanguage = "fr-FR"
+elif userTranslateLanguage == "4":
+    userTranslateLanguage = "it-IT"
+elif userTranslateLanguage == "5":
+    userTranslateLanguage = "de-DE"
 
-# Instantiates a client
-vision_client = vision.ImageAnnotatorClient()
+transLang = userTranslateLanguage[0:2]
 
-global image_client
-image_client = ImageAnnotatorClient()
-# The name of the image file to annotate
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'Earth.jpg')
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
-
-image = types.Image(content=content)
-
-# Performs label detection on the image file
-response = vision_client.label_detection(image=image)
-labels = response.label_annotations
-
-for label in labels:
-    translation = translate_client.translate(label.description, target_language = 'es')
+def translateStuff(targetLanguage, text):
+    global userTranslateLanguage
+    translate_client = translate.Client()
+    translation = translate_client.translate(text, target_language = targetLanguage)
     translatedText = translation["translatedText"]
-    print("\n" + translation["input"])
+    #print("\n" + translation["input"])
     print("Translation: " + translatedText)
+    synthesize_translated_text(translatedText, userTranslateLanguage)
 
+def synthesize_text(text, languageCode):
+    global num
+    """Synthesizes speech from the input string of text."""
+    from google.cloud import texttospeech
+    client = texttospeech.TextToSpeechClient()
 
-def detect_labels_url(uri):
+    input_text = texttospeech.types.SynthesisInput(text=text)
+
+    # Note: the voice can also be specified by name.
+    # Names of voices can be retrieved with client.list_voices().
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code=languageCode,
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
+
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+    response = client.synthesize_speech(input_text, voice, audio_config)
+
+    # The response's audio_content is binary.
+    with open('output' + str(num) + '.mp3', 'wb') as out:
+        out.write(response.audio_content)
+
+def synthesize_translated_text(text, languageCode):
+    global transNum
+    """Synthesizes speech from the input string of text."""
+    from google.cloud import texttospeech
+    client = texttospeech.TextToSpeechClient()
+
+    input_text = texttospeech.types.SynthesisInput(text=text)
+
+    # Note: the voice can also be specified by name.
+    # Names of voices can be retrieved with client.list_voices().
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code=languageCode,
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
+
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+    response = client.synthesize_speech(input_text, voice, audio_config)
+
+    # The response's audio_content is binary.
+    with open('translated_output' + str(transNum) + '.mp3', 'wb') as out:
+        out.write(response.audio_content)
+
+def labelsUrl(uri):
+    global num
+    global transNum
     """Detects labels in the file located in Google Cloud Storage or on the
     Web."""
     client = vision.ImageAnnotatorClient()
@@ -55,19 +98,13 @@ def detect_labels_url(uri):
     print('Labels:')
 
     for label in labels:
-        print(label.description)
+        print("\n" + label.description)
+        synthesize_text(label.description, 'en-US')
+        num += 1
+        translateStuff(transLang, label.description)
+        transNum += 1
 
-detect_labels_url("https://www.what-dog.net/Images/faces2/scroll0015.jpg")
 
+labelsUrl(userImage)
 
-def baseEncode(theImage):
-    image = open(theImage, 'rb')
-    image_read = image.read()
-    image_64_encode = base64.encodestring(image_read)
-
-    print(image_64_encode)
-
-    image_64_decode = base64.decodestring(image_64_encode)
-    image_result = open('Earth.jpg', 'wb')
-    image_result.write(image_64_decode)
 
